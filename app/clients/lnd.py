aef03729913ee4683d742a6c838bebf35ace9e2a
Mike Ghen <mike.ghen@gmail.com>
2019-05-23 19:53:36 -0400

    Added more LND RPC

--- app/clients/lnd.py
+++ app/clients/lnd.py
@@ -32,7 +32,7 @@ class LndLTC:
         metadata_plugin = MacaroonMetadataPlugin(macaroon)
         auth_creds = grpc.metadata_call_credentials(metadata_plugin)
         self._creds = grpc.composite_channel_credentials(cert_creds, auth_creds)
-        self.channel = grpcsecure_channel(
+        self.channel = grpc.secure_channel(
             'localhost', self._creds, options=[('grpc.max_receive_message_length', 1024*1024*50)]
         )
         self.lnd_stub = lnrpc.LightningStub(channel)
